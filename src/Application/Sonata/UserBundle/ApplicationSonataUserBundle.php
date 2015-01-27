e6719f84d03d98d40ac9987a3e121ceb7e19c2f3
boutitinizar <boutitinizar@gmail.com>
2015-01-27 18:49:49 +0100

    deplacer les view dee User bundel ver appliaction moduf theme login page

--- src/Application/Sonata/UserBundle/ApplicationSonataUserBundle.php
+++ src/Application/Sonata/UserBundle/ApplicationSonataUserBundle.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the <name> project.
  *
- * (c) <yourname> <youremail>
+ * (c) <boutiti nizar> <boutitinizar@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * References :
  *   bundles : http://symfony.com/doc/current/book/bundles.html
  *
- * @author <yourname> <youremail>
+ * @author <boutiti nizar> <boutitinizar@gmail.com>
  */
 class ApplicationSonataUserBundle extends Bundle
 {
